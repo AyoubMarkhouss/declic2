2c836502e5fc5e53a2a46c615c2ab1951cc7df56
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2025-01-31 11:34:12 +0100

    vw

--- src/pages/portfolio/project/expressrelais.tsx
+++ src/pages/portfolio/project/expressrelais.tsx
@@ -165,7 +165,7 @@ const Expressrelais = () => {
             {current} / {count}
           </div>
         </Carousel>
-        <TextAnimate
+        {/* <TextAnimate
           animation="blurInUp"
           by="character"
           className="fontmed laptop:text-7xl  bg-redeclic text-white  fontmed justify-center tablet:justify-start text-3xl w-full h-24  px-20 flex  items-center"
@@ -195,8 +195,8 @@ const Expressrelais = () => {
               />
             </div>
           </div>
-        </div>
-        <TextAnimate
+        </div> */}
+        {/* <TextAnimate
           animation="blurInUp"
           by="character"
           className="fontmed laptop:text-7xl  bg-redeclic text-white  fontmed justify-center tablet:justify-start text-3xl w-full h-24  px-20 flex  items-center"
@@ -226,8 +226,8 @@ const Expressrelais = () => {
               />
             </div>
           </div>
-        </div>
-        <TextAnimate
+        </div> */}
+        {/* <TextAnimate
           animation="blurInUp"
           by="character"
           className="fontmed laptop:text-7xl  bg-redeclic text-white  fontmed justify-center tablet:justify-start text-3xl w-full h-24  px-20 flex  items-center"
@@ -250,21 +250,7 @@ const Expressrelais = () => {
                 a moment of trust and reassurance for customers.
               </p>
             </div>
-            {/* <div className="flex items-center justify-center">
-              <Iphone15Pro
-                className="hidden laptop:block h-[500px] desktop:h-[550px]"
-                iframevid="https://www.youtube.com/embed/n1bdY9z7WpY?si=J9Mm59Q2bNR8l7rE?controls=0&modestbranding=1&autoplay=1&mute=1&rel=0&loop=1&playlist=_5EVk40zITs"
-              />
-             
-              <iframe
-                className="laptop:hidden  object-cover h-screen w-full overflow-hidden"
-                src="https://www.youtube.com/embed/n1bdY9z7WpY?si=uk63L4XKPj6goSym?controls=0&modestbranding=1&autoplay=1&mute=1&rel=0&loop=1&playlist=_5EVk40zITs"
-                title="Dailymotion video player"
-                frameBorder={0}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              />
-            </div> */}
+          
 
             <div className="flex justify-center items-center w-full">
               <Carousel
@@ -300,39 +286,8 @@ const Expressrelais = () => {
               </Carousel>
             </div>
           </div>
-        </div>
-        {/* <TextAnimate
-          animation="blurInUp"
-          by="character"
-          className="fontmed laptop:text-7xl  bg-redeclic text-white  fontmed justify-center tablet:justify-start text-4xl w-full h-24  px-20 flex  items-center"
-        >
-          The Aftermovie
-        </TextAnimate>
-        <div className="px-5 laptop:px-20 flex flex-col gap-y-5 py-5 laptop:py-20">
-          <div className="grid grid-cols-1 laptop:grid-cols-2 py-5 laptop:py-0 gap-y-5 gap-x-7">
-            <div className="flex flex-col text-justify justify-center laptop:text-start gap-y-5">
-              <p className="laptop:text-lg desktop:text-xl laptop:max-w-lg  desktop:max-w-full">
-                A day full of experiences and sharing. Participants had the
-                chance to discover and enjoy various workshops, indulge in tasty
-                moments at the food court, and explore a curated display of the
-                latest models and classic cars.Through discoveries, discussions,
-                and activities, each space allowed everyone to fully experience
-                the spirit of the festival. Thank you to everyone who helped
-                make this event a success. See you soon for more adventures!
-              </p>
-            </div>
-            <div className=" laptop:col-span-1 h-52 laptop:h-fit">
-              <iframe
-                className=" object-cover h-full laptop:h-[327px] desktop:h-[500px] w-full overflow-hidden"
-                src="https://www.youtube.com/embed/--8eCy6QWWs?si=oNjb0EtMhIJ0ytZM?controls=0&modestbranding=1&autoplay=1&mute=1&rel=0"
-                title="Dailymotion video player"
-                frameBorder={0}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              />
-            </div>
-          </div>
         </div> */}
+        
       </div>
       <Footer />
     </>
