37213c762cfc13a51e9afde923cad33cdfcf4104
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2025-02-13 15:10:39 +0100

    hh

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <ReactLenis>
-        <Head />
+        <Head></Head>
 
         <body className="antialiased">
           <Main />
